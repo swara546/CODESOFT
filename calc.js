e50847a1aeec6564ed1341b7cc8b830011c7a3ca
Swara <swaranatekar546@gmail.com>
2025-06-11 09:53:05 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,1 +1,17 @@
-
+  const result = document.getElementById('result');
+  function addValue(val) {
+    result.value += val;
+  }
+  function clr() {
+    result.value = '';
+  }
+  function del() {
+    result.value = result.value.slice(0, -1);
+  }
+  function calculate() {
+    try {
+      result.value = eval(result.value);
+    } catch {
+      result.value = 'Error';
+    }
+  }
